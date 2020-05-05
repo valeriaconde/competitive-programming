5eceb9dfed483938a71c55c1f317c703ea07dcb3
Valeria Conde <valeriaconde.96@gmail.com>
2020-05-05 16:06:28 -0700

    more fibonaccis

---
+++ last digit of sum of squares of fibonaccis/last digit of sum of squares of fibonaccis/last digit of sum of squares of fibonaccis.cpp
@@ -0,0 +1,35 @@
+// la princessval
+// arrancandolo con altura
+#include <iostream>
+#include <vector>
+#include <map>
+#define MOD 10
+using namespace std;
+typedef long long ll;
+
+map<ll, ll> f;
+
+ll fib(ll n) {
+    if(n == 0) return 0;
+    if(n == 1 || n == 2) return f[n] = 1;
+    
+    if(f[n]) return f[n];
+    
+    ll k = (n & 1) ? (n + 1) / 2 : n / 2;
+    
+    f[n] = (n & 1)? ((fib(k)*fib(k) % MOD) + (fib(k-1)*fib(k-1) % MOD))
+    : (((2*fib(k-1) % MOD) + fib(k)) % MOD)*fib(k) % MOD;
+    f[n] %= MOD;
+    return f[n];
+}
+
+int main() {
+    
+    ll n;
+    cin >> n;
+    cout << (fib(n) * fib(n + 1) + MOD) % MOD << endl;
+    
+    
+    return 0;
+}
+// le princessval template
--- pisano period.cpp
+++ pisano period.cpp
@@ -6,23 +6,34 @@
 //  Copyright © 2020 Valeria Conde. All rights reserved.
 //
 
+
+//compute 𝐹𝑛 modulo 𝑚, where 𝑛 may be really huge: up to 10^14
 #include <iostream>
+#include <vector>
+typedef long long ll;
 using namespace std;
 
-int main() {
-    int n;
-    cin >> n;
-    
-    int a = 0, b = 1, fib;
-    
-    for(int i = 2; i <= n; i++) {
-        fib = a + b;
-        a = b;
-        b = fib;
+vector<ll> period {0, 1};
+
+ll pisanoLength(ll f, ll mod) {
+    int cont = 0;
+    int anterior = 0;
+    int actual = 1;
+    while(cont < 2) {
+        int num = (anterior + actual) % mod;
+        period.push_back(num);
+        anterior = actual;
+        actual = num;
+        if (anterior == 0 && actual == 1) cont++;
     }
-    
-    cout << fib << endl;
-    
-    
+    return period.size() - 2;
+}
+
+int main() {
+    ll n, m;
+    cin >> n >> m;
+    int num = n % pisanoLength(n, m);
+    cout << period[num] << endl;
     return 0;
 }
+
